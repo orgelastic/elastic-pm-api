a538165d12c95693859321c9ec9fc07d0f868c80
Hieu Dong <donghuuhieu1520@gmail.com>
2023-09-18 00:29:39 +0700

    feat: split method of grpc server
    Signed-off-by: Hieu Dong <donghuuhieu1520@gmail.com>

--- grpc/user.go
+++ grpc/user.go
@@ -1,45 +1,14 @@
 package grpc
 
 import (
-	"context"
-	"database/sql"
-
 	"github.com/stewie1520/elasticpmapi/core"
 	proto_user "github.com/stewie1520/elasticpmapi/proto/src"
-	"github.com/stewie1520/elasticpmapi/usecases"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type UserServiceServer struct {
 	*proto_user.UnimplementedUserServiceServer
 	app core.App
-}
-
-// GetUserInfo implements proto_user.UserServiceServer.
-func (srv *UserServiceServer) GetUserInfo(ctx context.Context, req *proto_user.GetUserInfoRequest) (*proto_user.GetUserInfoResponse, error) {
-	q := usecases.NewGetUserByAccountIDQuery(srv.app)
-	q.AccountID = req.AccountId
-
-	user, err := q.Execute()
-
-	if err == sql.ErrNoRows {
-		return nil, status.Error(codes.NotFound, "user not found")
-	}
-
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	return &proto_user.GetUserInfoResponse{
-		Id:        user.ID,
-		FullName:  user.FullName,
-		AccountId: user.AccountId,
-		CreatedAt: timestamppb.New(user.CreatedAt.Time()),
-		UpdatedAt: timestamppb.New(user.UpdatedAt.Time()),
-	}, nil
 }
 
 func newUserServer(app core.App) *UserServiceServer {
